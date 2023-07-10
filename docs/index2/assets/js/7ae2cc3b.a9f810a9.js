89932dc645d3610e54521255b72d69309945d8d9
varuna.s <varuna.s@aample.in>
2023-07-10 12:05:40 +0000

    changes generic 2

--- docs/index2/assets/js/7ae2cc3b.a9f810a9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkslim_bp_doc_website=self.webpackChunkslim_bp_doc_website||[]).push([[280],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/index2/assets/js/c71552fc.b703632f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkslim_bp_doc_website=self.webpackChunkslim_bp_doc_website||[]).push([[512],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
